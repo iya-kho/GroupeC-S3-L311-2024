f7b185f818db86981498a2066cce7f2310d20c14
LeoAmaudruz <157134941+LeoAmaudruz@users.noreply.github.com>
2024-10-17 15:00:56 +0200

    Update index.html

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<!DOCTYPE html><html><head><meta charset="utf-8"><title>Timetitle> <link href="https://fonts.googleapis.comm/css?family=Aldrich" rel="stylesheet"> <link rel="stylesheet" href="asset/css/style.css"><script src="assets/js/script.js"></script></head><body><div id="wrapper"><div class="inside" id="wrapper"><div class="timer"><span id="hours">00</span><span id="minutes">00</span><span id="seconds">00</span></div><div class="controls"><button class="button pause"></button></div></div></div><body></html>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Timer</title> <!-- Correction du tag title -->
+    <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet"> <!-- Correction de l'URL -->
+    <link rel="stylesheet" href="assets/css/style.css"> <!-- Correction du chemin du fichier CSS -->
+    <script src="assets/js/script.js"></script> <!-- Correction du chemin du fichier JS -->
+</head>
+<body>
+    <div id="wrapper">
+        <div class="inside">
+            <div class="timer">
+                <span id="hours">00</span>
+                <span id="minutes">00</span>
+                <span id="seconds">00</span>
+            </div>
+            <div class="controls">
+                <button class="button pause"></button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
